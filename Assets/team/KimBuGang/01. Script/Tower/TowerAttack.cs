52f72e95c6f28a0ca6f85bc9fe0bc254d9ac9155
crispybugak <rhylawd@gmail.com>
2025-07-31 19:46:25 +0900

    feat : Test Tower

---
+++ Assets/team/KimBuGang/01. Script/Tower/TowerAttack.cs
@@ -0,0 +1,21 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class TowerAttack : MonoBehaviour
+{
+    public GameObject target;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 newPos = target.transform.position - transform.position;
+        float rotZ = Mathf.Atan2(newPos.y, newPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+}
