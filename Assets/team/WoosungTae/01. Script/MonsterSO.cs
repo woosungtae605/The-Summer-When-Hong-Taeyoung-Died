3d6d49338fbbb97091337506f8f6220b662bfb1b
woosungtae <a01076755795@gmail.com>
2025-07-31 11:56:34 +0900

    monster

---
+++ Assets/team/WoosungTae/01. Script/MonsterSO.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MonsterSO", menuName = "Scriptable Objects/MonsterSO")]
+public class MonsterSO : ScriptableObject
+{
+    public string name; // 몬스터 이름
+    public int hp; // 몬스터 체력
+    public float speed; // 몬스터 스피드
+    public int gold; //몬스터 죽을 때, 얻는 골드
+}
