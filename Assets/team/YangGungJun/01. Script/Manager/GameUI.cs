bfd35243ecb9a444d09bbefaf8673dd34df9f504
didrudwns <didrudwns123@gmail.com>
2025-08-01 19:46:28 +0900

    faet:Wave

--- Assets/team/YangGungJun/01. Script/Manager/GameUI.cs
+++ Assets/team/YangGungJun/01. Script/Manager/GameUI.cs
@@ -1,8 +1,31 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
+    [SerializeField] float CurretTime;
+    [SerializeField] float MaxTime;
+    [SerializeField] TextMeshProUGUI WaveText;
+    int count = 0;
+    private void Start()
+    {
+        SetText();
+    }
+    private void Update()
+    {
+        CurretTime += Time.deltaTime;
+        if (CurretTime >= MaxTime)
+        {
+            SetText();
+            CurretTime = 0;
+        }
+    }
+    public void SetText()
+    {
+        count++;
+        WaveText.text = $"{count}Wave";
+    }
     public void ReStartGame()
     {
         int NowScene = SceneManager.GetActiveScene().buildIndex;
