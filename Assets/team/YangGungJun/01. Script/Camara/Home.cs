12409777cf5ceed99e404be8c9b5bcddf0ce1d75
didrudwns <didrudwns123@gmail.com>
2025-08-02 07:05:51 +0900

    feat:GoldUI

--- Assets/team/YangGungJun/01. Script/Camara/Home.cs
+++ Assets/team/YangGungJun/01. Script/Camara/Home.cs
@@ -5,6 +5,12 @@ public class Home : MonoBehaviour
 {
     [SerializeField] int Hp = 10;
     [SerializeField] StageClearUI clearUI;
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Manager.manager.Sound.SetSoundSFX(5);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Target"))
@@ -16,6 +22,7 @@ public class Home : MonoBehaviour
     private void GetDmg(int dmg)
     {
         Hp -= dmg;
+        audioSource.Play();
         CamaraControll.camaraControl.DownCamara(1);
         if (Hp <= 0)
         {
