dc9d0560c49ef07efd0b6a924a084046dcf3d3f5
woosungtae <a01076755795@gmail.com>
2025-08-01 22:50:48 +0900

    feat : stage 2

---
+++ Assets/team/WoosungTae/01. Script/EnemyInitializer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using Unity.Behavior;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Agent))]
+[RequireComponent(typeof(PathMovement))]
+public class EnemyInitializer : MonoBehaviour
+{
+  
+    
+}
--- Assets/team/YangGungJun/01. Script/Camara/Home.cs
+++ Assets/team/YangGungJun/01. Script/Camara/Home.cs
@@ -6,7 +6,7 @@ public class Home : MonoBehaviour
     [SerializeField] int Hp = 10;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("target"))
+        if (collision.gameObject.CompareTag("Target"))
         {
             Destroy(collision.gameObject);
             GetDmg();
